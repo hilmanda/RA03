081829d269950c09d33d5dcd37a5996d5457fc02
BilhaqAD07 <87814270+BilhaqAD07@users.noreply.github.com>
2022-05-11 02:20:17 +0700

    split class into new files

---
+++ src/tile.py
@@ -0,0 +1,34 @@
+import pygame
+import cv2
+import os
+import random
+from src.menu import *
+from src.music import *
+from src.start import *
+from src.game import *
+
+WHITE = (255, 255, 255)
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, filename, x, y):
+        super().__init__()
+
+        self.name = filename.split('.')[0]
+
+        self.original_image = pygame.image.load('assets/cards/' + filename)
+
+        self.back_image = pygame.image.load('assets/cards/' + filename)
+        pygame.draw.rect(self.back_image, WHITE, self.back_image.get_rect())
+
+        self.image = self.back_image
+        self.rect = self.image.get_rect(topleft=(x, y))
+        self.shown = False
+
+    def update(self):
+        self.image = self.original_image if self.shown else self.back_image
+
+    def show(self):
+        self.shown = True
+
+    def hide(self):
+        self.shown = False
